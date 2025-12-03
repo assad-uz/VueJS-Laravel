c9488766f5d84c2fb8625c453c6dd8f50004ac3f
Assaduzzaman Shaon <assad.uz255@gmail.com>
2025-12-03 13:05:22 +0600

    first commit

---
+++ vueJS/src/views/ContactView.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="contact">
+    <h1>Contact us</h1>
+    <p>You will see your contact information on this page.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ContactView' // component name
+}
+</script>
+
+<style scoped>
+.contact {
+  padding: 20px;
+  text-align: center;
+}
+h1 {
+  color: #42b983;
+}
+</style>
