cd99b12636afaa64d5fcd66d8b8444b5834e1df9
Assaduzzaman Shaon <assad.uz255@gmail.com>
2025-12-07 08:48:59 +0600

    Update

---
+++ vue-backend/src/components/FooterComponent.vue
@@ -0,0 +1,26 @@
+<template>
+  <footer class="main-footer">
+    <p>&copy; 2025 আমার ভিউ অ্যাপ | সর্বস্বত্ব সংরক্ষিত</p>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'FooterComponent',
+};
+</script>
+
+<style scoped>
+.main-footer {
+  background-color: #333;
+  color: white;
+  padding: 1rem 2rem;
+  text-align: center;
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+}
+p {
+  margin: 0;
+}
+</style>
